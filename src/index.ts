ba37cd5e95588f07dd45fb79ba133210bdb4a2f3
nicolasmops <mopsnicolas@gmail.com>
2026-05-26 15:55:57 -0300

    tp finished

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,16 @@ let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
-let totalPorPersona: number = 0; // COMPLETAR
+let totalPorPersona: number;
+let descuento: number = 9/10;
+
+if(
+    esEstudiante === "si"
+){
+    totalPorPersona = Number(totalTexto) / Number(personasTexto) * descuento;
+} else {
+    totalPorPersona = Number(totalTexto) / Number(personasTexto);
+}
+
 console.log(`Total por persona: $${totalPorPersona}`);
+
